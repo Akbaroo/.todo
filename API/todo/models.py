296ce5acb916271f5f5540b94e1bc5ddf9ac3bcb
akbar <akbrg0872@gmail.com>
2025-02-20 09:36:57 -0800

    create model todo

--- API/todo/models.py
+++ API/todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+User = get_user_model()
+
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, models.CASCADE, verbose_name=_('user'))
+    title = models.CharField(_('title'), max_length=100)
+    description = models.TextField(_('description'))
+    start = models.DateTimeField(_('time start'), auto_now_add=True)
+    finish = models.DateTimeField(_('time finish'), null=True, blank=True)
